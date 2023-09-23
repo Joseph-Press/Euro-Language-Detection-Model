1145c056ae0e6ffa965d3475c716ca4dfaf2e6a8
Jeicex1 <99057897+Jeicex1@users.noreply.github.com>
2023-09-23 03:32:34 -0700

    created europarl project

--- README.md
+++ README.md
@@ -2,10 +2,7 @@
 
 ## Description
 
-This project aims to build a language detection model using the fastText library. The model is trained on the European Parliament Proceedings Parallel Corpus and can identify one of 21 languages.
-
-## Languages Supported
-
+This project aims to build a language detection model using the fastText library. The model is trained on a modified version of the European Parliament Proceedings Parallel Corpus and can identify 11 languages.
 
 - Danish (da)
 - Dutch (nl)
@@ -24,23 +21,15 @@ This project aims to build a language detection model using the fastText library
 1. Clone the repository.
 2. Navigate to the project directory.
 3. Install the requirements using `pip install -r requirements.txt`.
-4. Run the scripts in the `scripts` folder to prepare the data and train the model.
+4. Download the data from [here](http://www.statmt.org/europarl/). Alternatively use the csv files in the `data` folder.
+5. Run the scripts in the `scripts` folder to prepare the data and train the model.
 
 ## Scripts
 
-- `prepare_data.sh`: Downloads and prepares the European Parliament corpus.
+- `evaluate_model.py`: Evaluates the trained model and makes predictions.
+- `predict.py`: Makes predictions on the test data.
 - `preprocess_data.py`: Cleans and preprocesses the text data.
 - `train_model.sh`: Trains the fastText model.
-- `evaluate_model.py`: Evaluates the trained model and makes predictions.
-
-## Usage
-
-To train and evaluate the model, execute the following commands in order:
-
-1. `bash scripts/prepare_data.sh`
-2. `python scripts/preprocess_data.py`
-3. `bash scripts/train_model.sh`
-4. `python scripts/evaluate_model.py`
 
 ## Requirements
 
--- scripts/preprocess_data.py
+++ scripts/preprocess_data.py
@@ -1,62 +1,66 @@
+import os
 import pandas as pd
 import re
 import numpy as np
 
-# clean text
+# Clean text
 def clean_text(text):
     return re.sub(r'<.*?>', '', text).lower()
 
-# convert corpus to DataFrame
+# Convert corpus to DataFrame
 def corpus_to_df(filepath, lang):
     df = pd.read_table(filepath, error_bad_lines=False, header=None, names=['text'])
     df['lang'] = lang
     return df[['lang', 'text']]
 
-# prepare text for fastText
+# Prepare text for fastText
 def normalize_text(row):
     label = '__label__' + row['lang']
     text = row['text']
     return f"{label} , {text}"
 
-# convert each language file to a CSV
-languages = [lang.replace('.txt', '') for lang in os.listdir('txt/') if lang.endswith('.txt')]
+# Create directory for temporary CSV files
+os.makedirs('data/csv/', exist_ok=True)
+
+# Convert each language file to a CSV
+languages = [lang.replace('.txt', '') for lang in os.listdir('data/EuroparlRaw/') if lang.endswith('.txt')]
 
 for lang in languages:
-    filepath = f'txt/{lang}.txt'
+    filepath = f'data/EuroparlRaw/{lang}.txt'
     print(f"Processing {filepath}...")
     
-    # clean text
+    # Clean text
     with open(filepath, 'r') as f:
         cleaned_text = clean_text(f.read())
     
-    # save text
+    # Save cleaned text
     cleaned_filepath = filepath.replace('.txt', '-cleaned.txt')
     with open(cleaned_filepath, 'w') as f:
         f.write(cleaned_text)
 
-    # convert to DataFrame and save as CSV
+    # Convert to DataFrame and save as CSV
     df = corpus_to_df(cleaned_filepath, lang)
     df.to_csv(f'data/csv/{lang}.csv', index=False, header=False)
 
     print(f"Finished processing {filepath}.")
 
-# concatenate samples from each language to create a large dataset
+# Concatenate samples from each language to create a large dataset
 samples_per_lang = 40000
 combined_df = pd.concat([
     pd.read_csv(f"data/csv/{lang}.csv", names=['lang', 'text']).sample(samples_per_lang, random_state=1)
     for lang in languages
 ], ignore_index=True)
 
-# shuffle, normalize, and split the dataset
+# Shuffle, normalize, and split the dataset
 combined_df = combined_df.sample(frac=1, random_state=1).reset_index(drop=True)
 combined_df['normalized'] = combined_df.apply(normalize_text, axis=1)
 
-# slpit into training and test sets
+# Split into training and test sets
 split_idx = int(0.75 * len(combined_df))
 train_df = combined_df.loc[:split_idx, 'normalized']
 test_df = combined_df.loc[split_idx:, 'normalized']
 
-# save training and test sets
+# Save training and test sets
 train_df.to_csv('data/europarl.train', index=False, header=False)
 test_df.to_csv('data/europarl.test', index=False, header=False)
 
